f06543509b31e2561e0adb19df0a756253b5dc9b
박영웅 <thesecon@gmail.com>
2016-11-09 22:50:58 +0900

    깜지 입력 기능(검증) 추가 - 카운터가 0에서 시작한다. - 올바른 문구를 입력하고 '엔터'를 누르면 카운터가 증가한다. - 올바른 문구를 입력하면 글자색이 녹색이 된다. - 틀린 문구를 입력하고 '엔터'를 누르면 글자가 흔들린다.
    CSS 변경
    - 틀린 문구를 입력하면 input 요소에 `.right`를 추가하여 색상 변경
    
    modules.js.json 변경
     - `ggamji.js` 추가
    
    create ggamji.ejs and add actions #3

--- app/js/common.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-  var ggamji = new Ggamji();
-});
--- app/js/sources/ggamji/ggamji.js
+++ app/js/sources/ggamji/ggamji.js
@@ -6,6 +6,12 @@ Ggamji.prototype.init = function () {
   var _that = this;
 
   _that.$input = $('#enter_data');
+  _that.inputData = null;
+  _that.inputData = null;
+  _that.$target = $('#target');
+  _that.targetText = _that.$target.text();
+  _that.$counter = $('#counter');
+  _that.countNumber = 0;
 
   _that.initEvent();
 };
@@ -13,15 +19,79 @@ Ggamji.prototype.init = function () {
 Ggamji.prototype.initEvent = function () {
   var _that = this;
 
+  _that.startGgamji();
   _that.inputEvent();
+};
+
+Ggamji.prototype.startGgamji = function () {
+  var _that = this;
+
+  $(document).on({
+    keydown: function () {
+      _that.$input.focus();
+    }
+  });
 };
 
 Ggamji.prototype.inputEvent = function () {
   var _that = this;
 
   _that.$input.on({
-    keydown: function (e) {
-      console.log(e.which);
+    keydown: function (event) {
+      var $self = $(this);
+      var inputData = $self.val();
+
+      console.log(event.which);
+
+      switch (event.which) {
+        case 13:  // enter key
+          if (inputData == _that.targetText) {
+            _that.iterationCount();
+            _that.resetInput();
+          } else {
+            _that.wrongAnswerEvent()
+          }
+          break;
+      }
+    },
+    keyup: function (event) {
+      _that.rightAnswerEvent($(this));
     }
   });
+};
+
+Ggamji.prototype.rightAnswerEvent = function ($self) {
+  var _that = this;
+  var inputData = $self.val();
+
+  if (inputData === _that.targetText) {
+    _that.$input.addClass('right');
+  } else {
+    _that.$input.removeClass('right');
+  }
+};
+
+Ggamji.prototype.wrongAnswerEvent = function () {
+  var _that = this;
+  var duration = 50;
+
+  _that.$input
+    .animate({ marginLeft: -10 }, duration)
+    .animate({ marginLeft: 10 }, duration)
+    .animate({ marginLeft: -10}, duration)
+    .animate({ marginLeft: 10}, duration)
+    .animate({ marginLeft: 0 }, duration / 2);
+};
+
+Ggamji.prototype.iterationCount = function () {
+  var _that = this;
+
+  _that.countNumber++;
+  _that.$counter.text(_that.countNumber);
+};
+
+Ggamji.prototype.resetInput = function () {
+  var _that = this;
+
+  _that.$input.val('');
 };
